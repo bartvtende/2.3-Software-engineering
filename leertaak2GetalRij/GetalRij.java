3cd40cb9e601c5a066df61322071b3d8611b56d6
Bart van 't Ende <bartvtende@gmail.com>
2015-02-17 22:51:22 +0100

    Added three search algorithms

--- leertaak2GetalRij/GetalRij.java
+++ leertaak2GetalRij/GetalRij.java
@@ -2,54 +2,105 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-
 public class GetalRij {
 	private int[] getallen;
-	
-	public GetalRij( int aantal, int max ){
-		// Belangrijke aanname: aantal < max, anders kunnen de getallen niet uniek zijn.
+
+	public GetalRij(int aantal, int max) {
+		// Belangrijke aanname: aantal < max, anders kunnen de getallen niet
+		// uniek zijn.
 		getallen = new int[aantal];
-		vulArrayMetUniekeWaarden( aantal, max );
+		vulArrayMetUniekeWaarden(aantal, max);
 	}
 
 	private void vulArrayMetUniekeWaarden(int aantal, int max) {
 		// Vul een hulplijst met getallen 0, ..., max
-		ArrayList hulpLijst = new ArrayList( max );
-		for ( int i=0; i<max; i++){
-			hulpLijst.add( i );
+		ArrayList hulpLijst = new ArrayList(max);
+		for (int i = 0; i < max; i++) {
+			hulpLijst.add(i);
 		}
-		
+
 		// Stop 'aantal' random waarden in getallen
 		Random r = new Random();
-		for ( int i=0; i<aantal; i++){
-			// Het omzetten van Integer naar int gaat sinds Java 1.5 automatisch (unboxing).
-			int getal = (Integer) (hulpLijst.remove( r.nextInt( hulpLijst.size())));
+		for (int i = 0; i < aantal; i++) {
+			// Het omzetten van Integer naar int gaat sinds Java 1.5 automatisch
+			// (unboxing).
+			int getal = (Integer) (hulpLijst
+					.remove(r.nextInt(hulpLijst.size())));
 			getallen[i] = getal;
 		}
 	}
-	
-	public boolean zitErinA( int zoekWaarde ){
+
+	/**
+	 * Opdracht 3 leertaak 2
+	 * 
+	 * @param zoekWaarde
+	 * @return
+	 */
+	public boolean zitErinA(int zoekWaarde) {
+		int counter = 0;
+		boolean isFound = false;
+		while (counter < getallen.length) {
+			if (zoekWaarde == getallen[counter])
+				isFound = true;
+			counter++;
+		}
+		return isFound;
+	}
+
+	/**
+	 * Opdracht 4 leertaak 2
+	 * 
+	 * @param zoekWaarde
+	 * @return
+	 */
+	public boolean zitErinB(int zoekWaarde) {
+		int counter = 0;
+		while (counter < getallen.length) {
+			if (zoekWaarde == getallen[counter])
+				return true;
+			counter++;
+		}
 		return false;
 	}
 
-	public boolean zitErinB( int zoekWaarde ){
+	/**
+	 * Binary search algorithm
+	 * 
+	 * @param zoekWaarde
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public boolean zitErinC(int zoekWaarde, int min, int max) {
+		if (max < min) {
+			return false;
+		}
+		else {
+			int mid = (min + max) / 2;
+
+			if (getallen[mid] > zoekWaarde)
+				return zitErinC(zoekWaarde, min, mid - 1);
+			else if (getallen[mid] < zoekWaarde)
+				return zitErinC(zoekWaarde, mid + 1, max);
+			else
+				return true;
+		}
+	}
+
+	public boolean zitErinD(int zoekWaarde) {
 		return false;
 	}
 
-	public boolean zitErinC( int zoekWaarde ){
-		return false;
+	public void sorteer() {
+		Arrays.sort(getallen);
 	}
 
-	public boolean zitErinD( int zoekWaarde ){
-		return false;
+	public int[] getGetallen() {
+		return getallen;
 	}
-	
-	public void sorteer(){
-		Arrays.sort( getallen);
-	}
-	
-	public void print(){
-		for( int i=0; i<getallen.length; i++)
+
+	public void print() {
+		for (int i = 0; i < getallen.length; i++)
 			System.out.println(getallen[i]);
 	}
 
--- leertaak2GetalRij/SnelheidOefening.java
+++ leertaak2GetalRij/SnelheidOefening.java
@@ -1,15 +1,26 @@
-
 public class SnelheidOefening {
 
 	/**
 	 * @param args
 	 */
-	public static void main( String[] args){
-		GetalRij gr = new GetalRij( 100000, 200000);
+	public static void main(String[] args) {
+		GetalRij gr = new GetalRij(100000, 200000);
+		System.out.println("Starttijd A: " + tijd());
+		System.out.println(gr.zitErinA(0));
+		System.out.println("Eindtijd A: " + tijd());
+		System.out.println("Starttijd B: " + tijd());
+		System.out.println(gr.zitErinB(0));
+		System.out.println("Eindtijd B: " + tijd());
+		gr.sorteer();
+		int getallenLength = gr.getGetallen().length;
+		System.out.println("Starttijd C: " + tijd());
+		System.out.println(gr.zitErinC(0, 0, getallenLength-1));
+		System.out.println("Eindtijd C: " + tijd());
+
 	}
 
 	// Hulpmethode voor tijdsbepaling
-	private static long tijd(){
+	private static long tijd() {
 		return System.currentTimeMillis();
 	}
 
