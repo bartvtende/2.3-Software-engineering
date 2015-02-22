3539dde398a0cb68fd22828007ad2a3e77793f75
Figueus <j-c-s-s@hotmail.com>
2015-02-22 20:53:23 +0100

    fuck yeah, progress!

---
+++ leertaak2Classifier/src/start/Start17.java
@@ -0,0 +1,24 @@
+package start;
+
+import javax.swing.JFrame;
+
+import classifier.DecisionTree;
+import classifier.Node;
+import classifier.TrainingSetReader;
+import view.TreePopup;
+
+public class Start17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//TrainingSetReader trainingSet = new TrainingSetReader();
+
+		//DecisionTree tree = trainingSet.getDecisionTree();
+		DecisionTree tree = null;
+		TreePopup pop = new TreePopup();
+		pop.pack();
+	}
+	
+	
+
+}
---
+++ leertaak2Classifier/src/view/TreePopup.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import classifier.DecisionTree;
+
+public class TreePopup extends JFrame {
+	final String title = "";
+	static GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
+	
+	public TreePopup(){
+		
+	    
+	    setResizable(false);
+	    setUndecorated(true);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    device.setFullScreenWindow(this);
+	    JPanel screen = new JPanel(); //Screen is a JPanel.
+	    this.setPreferredSize(new Dimension(1920,1080));
+	    screen.setSize(getWidth(), getHeight());
+	    add(screen);
+	    setVisible(true);
+	    Container cp = this.getContentPane();
+	    System.out.println(cp.getWidth()+"()"+cp.getHeight());
+	}
+}
