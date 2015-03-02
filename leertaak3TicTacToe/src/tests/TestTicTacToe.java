4cf2fa4b14cb6c0d64bf6a133f3ae237fedfb5e2
Bart van 't Ende <bartvtende@gmail.com>
2015-03-02 17:04:04 +0100

    Started on assignment 6 and deleted a unnecessary file from leertaak 2

---
+++ leertaak3TicTacToe/src/tests/TestTicTacToe.java
@@ -0,0 +1,31 @@
+package tests;
+
+import junit.framework.TestCase;
+
+import ttt.*;
+
+public class TestTicTacToe extends TestCase {
+
+	private TicTacToeGame ticTacToe;
+
+	public TestTicTacToe(String arg0) {
+		super(arg0);
+	}
+	
+	/**
+	 * Test: positionValue(), isAWin(), chooseMove()
+	 * 
+	 * De unittest van chooseMove moet testen dat de methode de beste zet kiest.
+	 */
+	public testTicTacToe() {
+		TicTacToeGame ticTacToe = new TicTacToeGame();
+		
+		// Simulate a game
+		
+		
+		
+		
+		
+	}
+	
+}
