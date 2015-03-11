a2953002e724eff1f475eddc60c3af50aadaaedf
Figueus <j-c-s-s@hotmail.com>
2015-03-11 14:46:45 +0100

    reverse print

---
+++ leertaak4NumbersPrint/src/reverse/Numbersprint.java
@@ -0,0 +1,15 @@
+package reverse;
+
+public class Numbersprint {
+
+	
+	
+	public Numbersprint(){
+		new Task(1).start();
+		new Task(2).start();
+		new Task(3).start();
+		new Task(4).start();
+	}
+	
+	
+}
